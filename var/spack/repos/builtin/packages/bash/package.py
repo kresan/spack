052600af705939b80e23c956d145d406b7080280
Dmytro Kresan <d.kresan@gsi.de>
2019-11-25 13:03:05 +0100

    Use new location for GNU packages

--- var/spack/repos/builtin/packages/bash/package.py
+++ var/spack/repos/builtin/packages/bash/package.py
@@ -10,7 +10,7 @@ class Bash(AutotoolsPackage):
     """The GNU Project's Bourne Again SHell."""
 
     homepage = "https://www.gnu.org/software/bash/"
-    url      = "https://ftpmirror.gnu.org/bash/bash-4.4.tar.gz"
+    url      = "https://ftp.gnu.org/gnu/bash/bash-4.4.tar.gz"
 
     version('5.0', sha256='b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d')
     version('4.4.12', '7c112970cbdcadfc331e10eeb5f6aa41')
--- var/spack/repos/builtin/packages/libunistring/package.py
+++ var/spack/repos/builtin/packages/libunistring/package.py
@@ -11,7 +11,7 @@ class Libunistring(AutotoolsPackage):
     and for manipulating C strings according to the Unicode standard."""
 
     homepage = "https://www.gnu.org/software/libunistring/"
-    url      = "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.10.tar.xz"
+    url      = "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.10.tar.xz"
     version('0.9.10', sha256='eb8fb2c3e4b6e2d336608377050892b54c3c983b646c561836550863003c05d7')
     version('0.9.9',  sha256='a4d993ecfce16cf503ff7579f5da64619cee66226fb3b998dafb706190d9a833')
     version('0.9.8',  sha256='7b9338cf52706facb2e18587dceda2fbc4a2a3519efa1e15a3f2a68193942f80')
